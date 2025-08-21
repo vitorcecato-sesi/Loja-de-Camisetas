8c21650c52b75cc2c930478e2d250045b1507422
Lucas C <lucascasagrandesesi@gmail.com>
2025-08-21 16:00:35 -0300

    Junção das telas e finalização do projeto (falta configuração)

--- App.js
+++ App.js
@@ -4,13 +4,19 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import TelaListaDeCamisas from './screens/Catalogo';
 import ListaDetalhesProdutos from './screens/DetalhesProdutos';
+import TelaLogin from './screens/TelaLogin';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Catalogo">
+            <Stack.Navigator initialRouteName="Login">
+              <Stack.Screen
+                    name="Login"
+                    component={TelaLogin}
+                    options={{ title: 'Login' }}
+                />
                 <Stack.Screen
                     name="Catalogo"
                     component={TelaListaDeCamisas}
--- screens/TelaLogin.js
+++ screens/TelaLogin.js
@@ -54,7 +54,7 @@ function TelaLogin({ navigation }) {
     // Simula um atraso de 3 segundos antes de navegar
     setTimeout(() => {
       setCarregando(false);
-      navigation.navigate('TelaListaProdutos'); // Navega para a próxima tela
+      navigation.navigate('Catalogo'); // Navega para a próxima tela
     }, 3000); 
   }
 
